db68b9cd1e2cb3a407c4c28c176a23f8af4419b8
Mansoo Kim <mansoo0621@mju.ac.kr>
2019-06-05 11:34:50 +0900

    Write in LectureTable_NoActionChange

--- Procedure_Project/src/home/controllers/TableController.java
+++ Procedure_Project/src/home/controllers/TableController.java
@@ -1,19 +1,46 @@
 package home.controllers;
 
 import home.model.LectureModel;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class TableController implements Initializable{
-	@FXML TableView<LectureModel> lectureData;
+public class TableController implements Initializable{	
+	@FXML TableView<LectureModel> lectureTable;
+	
+	@FXML TableColumn<LectureModel, Integer> numberColumn;
+	@FXML TableColumn<LectureModel, String> nameColumn;
+	@FXML TableColumn<LectureModel, String> professorColumn;
+	@FXML TableColumn <LectureModel, Integer>creditColumn;
+	@FXML TableColumn <LectureModel, String>timeColumn;	
+	
+	private ObservableList<LectureModel> lectureList= FXCollections.observableArrayList();
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
+		loadLecture();
+	}
+	
+	private ObservableList<LectureModel> lectureModel = FXCollections.observableArrayList(
+			new LectureModel(3214,"객체지향적사고와프로그래밍","최성운",3,"월수9:00-10:30")
+	);
+	
+	private void loadLecture() {
+		numberColumn.setCellValueFactory(new PropertyValueFactory<>("Number"));
+		nameColumn.setCellValueFactory(new PropertyValueFactory<>("Name"));
+		professorColumn.setCellValueFactory(new PropertyValueFactory<>("Professor"));
+		creditColumn.setCellValueFactory(new PropertyValueFactory<>("Credit"));
+		timeColumn.setCellValueFactory(new PropertyValueFactory<>("Time"));
 		
+		lectureTable.setItems(lectureModel);
 	}
 
 }
--- Procedure_Project/src/home/model/LectureModel.java
+++ Procedure_Project/src/home/model/LectureModel.java
@@ -1,14 +1,16 @@
 package home.model;
 
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class LectureModel {
-	private SimpleIntegerProperty number;
-	private SimpleStringProperty name;
-	private SimpleStringProperty professor;
-	private SimpleIntegerProperty credit;
-	private SimpleStringProperty time;
+	private final IntegerProperty number;
+	private final StringProperty name;
+	private final StringProperty professor;
+	private final IntegerProperty credit;
+	private final StringProperty time;
 	
 	public LectureModel(int number, String name, String professor, int credit, String time) {
 		this.number = new SimpleIntegerProperty(number);
@@ -23,7 +25,11 @@ public class LectureModel {
 	}
 	
 	public void setNumber(int number) {
-		this.number = new SimpleIntegerProperty(number);
+		this.number.set(number);
+	}
+	
+	public IntegerProperty numberProperty() {
+		return number;
 	}
 	
 	public String getName() {
@@ -31,7 +37,11 @@ public class LectureModel {
 	}
 	
 	public void setName(String name) {
-		this.name = new SimpleStringProperty(name);
+		this.name.set(name);
+	}
+	
+	public StringProperty nameProperty() {
+		return name;
 	}
 	
 	public String getProfessor() {
@@ -39,7 +49,11 @@ public class LectureModel {
 	}
 	
 	public void setProfessor(String professor) {
-		this.professor = new SimpleStringProperty(professor);
+		this.professor.set(professor);
+	}
+	
+	public StringProperty professorProperty() {
+		return professor;
 	}
 	
 	public int getCredit() {
@@ -47,7 +61,11 @@ public class LectureModel {
 	}
 	
 	public void setCredit(int credit) {
-		this.credit = new SimpleIntegerProperty(credit);
+		this.credit.set(credit);
+	}
+	
+	public IntegerProperty creditProperty() {
+		return credit;
 	}
 	
 	public String getTime() {
@@ -55,6 +73,10 @@ public class LectureModel {
 	}
 	
 	public void setTime(String time) {
-		this.time = new SimpleStringProperty(time);
+		this.time.set(time);
+	}
+	
+	public StringProperty timeProperty() {
+		return time;
 	}
 }
